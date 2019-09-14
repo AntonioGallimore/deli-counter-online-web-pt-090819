be792e0bf437a1320e958bb410542f9aa981eb12
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-09-14 14:34:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,13 @@
 # Write your code here.
 def line
 end
+let(:katz_deli) { [] }
+let(:other_deli) { ["Logan", "Avi", "Spencer"] }
+describe "#line" do
+  context "there is nobody in line" do
+    it "should say the line is empty" do
+    
+      expect($stdout).to receive(:puts).with("The line is currently empty.")
+      line(katz_deli)
+    end
+  end
